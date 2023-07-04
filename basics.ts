34eab743ca9ac51ee4378f9436f8ad0ab9ff9403
juhee <nohjuhee227@gmail.com>
2023-07-04 23:04:59 +0900

    LEARN: 배열과 객체 type(428)

--- basics.ts
+++ basics.ts
@@ -15,5 +15,27 @@ let userName: string = 'nora';
 let isInstructor: boolean;
 isInstructor = true;
 
-let hobbies: null;
+// let hobbies: null;
 // hobbies = 'exercise';
+
+
+/* More complex types => array & object */
+
+let hobbies: string[]; // 문자열 배열 타입
+let ages: number[]; // 숫자형 배열 타입
+let isTrue: boolean[]; // 숫자형 배열 타입
+hobbies = ['수영하기', '운동하기', '요리하기'];
+ages = [1, 2, 3, 4];
+isTrue = [true, false, false];
+
+// type을 지정하지 않는다면 기본 'any'로 지정됨 -> 어떤 값이든 지정될 수 있다는 의미
+let person;
+let user: {
+  name: string;
+  age: number;
+}
+
+let users: {
+  name: string;
+  age: number;
+}[] = [{ name: 'nora', age: 28 }, { name: 'Max', age: 30 }];
