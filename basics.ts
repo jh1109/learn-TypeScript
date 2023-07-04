3d188815ea8e3040d3ed46adfa3c9b28d5ce086e
juhee <nohjuhee227@gmail.com>
2023-07-04 22:51:24 +0900

    LEARN: primitives type

---
+++ basics.ts
@@ -0,0 +1,19 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+/* 
+<Primitives type>
+-> number, string, boolean, null, undefined, symbol 
+*/
+let age: number;
+age = 12.3;
+// age = '12';
+
+let userName: string = 'nora';
+
+let isInstructor: boolean;
+isInstructor = true;
+
+let hobbies: null;
+// hobbies = 'exercise';
