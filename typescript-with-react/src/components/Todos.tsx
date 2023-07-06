a8662cffe8c49409f755e746fb84b606d70a03ae
juhee <nohjuhee227@gmail.com>
2023-07-06 17:34:21 +0900

    TRY: todoItem 컴포넌트 만들어보기(439)

--- typescript-with-react/src/components/Todos.tsx
+++ typescript-with-react/src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Todo from '../models/todo';
+import TodoItem from './TodoItem';
 
 // React.FC: @types/react 패키지에 정의되어 있는 타입(generic type)
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
-      {props.items.map(item => <li key={item.id}>{item.text}</li>)}
+      {props.items.map(item => <TodoItem item={item} />)}
     </ul>
   );
 };
