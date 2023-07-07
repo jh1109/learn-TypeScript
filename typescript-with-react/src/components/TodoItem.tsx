715a92127d9a338fec509768336d7f06537f0551
juhee <nohjuhee227@gmail.com>
2023-07-08 00:00:50 +0900

    LEARN: 스타일 추가(444)

--- typescript-with-react/src/components/TodoItem.tsx
+++ typescript-with-react/src/components/TodoItem.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import classes from './TodoItem.module.css'
+
 // React.FC는 key와 같이 특별한 역할을 가진 props를 사용할 수 있게 해준다.
 // 그래서 TodoItem에 key를 props로 지정할 수 있는 것이다.
 const TodoItem: React.FC<{ text: string }> = (props) => {
   return (
-    <li>{props.text}</li>
+    <li className={classes.item}>{props.text}</li>
   )
 };
 
--- typescript-with-react/src/components/Todos.tsx
+++ typescript-with-react/src/components/Todos.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Todo from '../models/todo';
 import TodoItem from './TodoItem';
 
+import classes from './Todos.module.css'
+
 // React.FC: @types/react 패키지에 정의되어 있는 타입(generic type)
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map(item => <TodoItem key={item.id} text={item.text} />)}
     </ul>
   );
